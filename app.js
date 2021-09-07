8f19a2ebb8d55e5b7f61b6f84da35a68b46f1fa4
Vitalii <v.mirniy@icloud.com>
2021-09-07 11:20:53 +0300

    change event days

--- app.js
+++ app.js
@@ -1,47 +1,31 @@
 const getRandChoice = (choices) => {
-
     return choices[getRandom(choices.length)];
-
 };
 
 const getRandom = n => {
     return Math.floor(Math.random() * n);
 };
 
-const getTrainingDays = event => {
-    let days;
-    if (event === 'Marathon') {
-        days = 50;
-    } else if (event === 'Triathlon') {
-        days = 100;
-    } else if (event === 'Pentathlon') {
-        days = 200;
-    }
-
-    return days;
+const logEvent = (name, competition) => {
+    console.log(`${name}'s event is: ${competition.nameOf}`);
 };
 
-
-const logEvent = (name, event) => {
-    console.log(`${name}'s event is: ${event}`);
+const logTime = (name, competition) => {
+    console.log(`${name}'s time to train is: ${competition.days} days`);
 };
 
-const logTime = (name, days) => {
-    console.log(`${name}'s time to train is: ${days} days`);
-};
-
-const events = ['Triathlon', 'Marathon', 'Pentathlon'];
+const events = [
+    {days: 50, nameOf: 'Triathlon'},
+    {days: 100, nameOf: 'Marathon'},
+    {days: 200, nameOf: 'Pentathlon'}
+];
 
 const event = getRandChoice(events);
-const days = getTrainingDays(event);
 
 let name = 'Nala';
 logEvent(name, event);
-logTime(name, days);
-
-const event2 = getRandChoice(events);
-const days2 = getTrainingDays(event2);
+logTime(name, event);
 
 const name2 = 'Warren';
-logEvent(name2, event2);
-logTime(name2, days2);
+logEvent(name2, event);
+logTime(name2, event);
