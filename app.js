e3c63e0afc0357ac0f10f363e35283757750d219
Vitalii <v.mirniy@icloud.com>
2021-09-08 00:22:47 +0300

    O(1)

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const test = {
         {days: 50, sportEvent: 'Triathlon'},
         {days: 100, sportEvent: 'Marathon'},
         {days: 200, sportEvent: 'Pentathlon'},
+        {days: 30, sportEvent: 'Swoosh'},
+        {days: 40, sportEvent: 'Football'},
+        {days: 80, sportEvent: 'Tennis'},
+        {days: 50, sportEvent: 'Baseball'},
+        {days: 60, sportEvent: 'Crossfit'},
+        {days: 70, sportEvent: 'Gym'},
     ]
 }
 
